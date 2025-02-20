e53ab63352b26456099e7e4294426fe0dfbb7fdf
Howard <z112517z@gmail.com>
2025-02-20 20:09:33 +0800

    feat: 影像辨識
    調整項目:
    1. 新增影像辨識功能

--- app/api/vision-ai/route.js
+++ app/api/vision-ai/route.js
@@ -1,22 +1,36 @@
 import openai from "@/services/openai";
 
 export async function POST(req) {
-    const body = await req.json();
-    console.log("body:", body);
-    // TODO: 透過base64讓AI辨識圖片
-    // 文件連結：https://platform.openai.com/docs/guides/vision?lang=node
-    const systemPrompt = ``;
-    const propmpt = ``;
+  const body = await req.json();
+  console.log("body:", body);
 
-    const openAIReqBody = {
-        messages: [
-            { "role": "system", "content": systemPrompt },
-            { "role": "user", "content": propmpt }
+  const { base64 } = body;
+  // TODO: 透過base64讓AI辨識圖片
+  // 文件連結：https://platform.openai.com/docs/guides/vision?lang=node
+  const systemPrompt = `告訴我這張圖片有甚麼`;
+  const propmpt = base64;
+
+  const openAIReqBody = {
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "圖片裡有甚麼?請用繁體中文回答",
+          },
+          {
+            type: "image_url",
+            image_url: { url: base64 },
+          },
         ],
-        model: "gpt-4o-mini",
-    };
-    // const completion = await openai.chat.completions.create(openAIReqBody);
-    // console.log("completion:", completion);
+      },
+    ],
+    model: "gpt-4o-mini",
+  };
+  const completion = await openai.chat.completions.create(openAIReqBody);
+  const aiText = completion.choices[0].message.content;
+  console.log("completion:", aiText);
 
-    return Response.json({ message: "Success" });
+  return Response.json({ message: "Success", aiText });
 }
--- app/vision/page.js
+++ app/vision/page.js
@@ -4,40 +4,71 @@ import { useState } from "react";
 import axios from "axios";
 import CurrentFileIndicator from "@/components/CurrentFileIndicator";
 import PageHeader from "@/components/PageHeader";
-import { faEye } from "@fortawesome/free-solid-svg-icons"
-
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 export default function Vision() {
-    // 是否在等待回應
-    const [isWaiting, setIsWaiting] = useState(false);
+  // 是否在等待回應
+  const [isWaiting, setIsWaiting] = useState(false);
+  const [aiText, setAiText] = useState("");
+  const [aiImageUrl, setAiImageUrl] = useState("");
+  const changeHandler = (e) => {
+    e.preventDefault();
+    // get user upload file
+    // 取得使用者上傳的檔案
+    // 取得使用者上傳的檔案
+    const file = e.target.files[0];
+    // 建立FileReader物件來讀取檔案
+    const reader = new FileReader();
+    // 設定預覽圖片的URL,使用URL.createObjectURL產生一個暫時的URL
+    setAiImageUrl(URL.createObjectURL(file));
+    setAiText("辨識中...");
+    reader.onload = () => {
+      const base64Image = reader.result;
+      
+      // invoke api
+      axios
+        .post("/api/vision-ai", { base64: base64Image })
+        .then((res) => {
+          setAiText(res.data.aiText);
+        })
+        .catch((err) => {
+          alert("ai image vision error...");
+        });
+    };
+    reader.readAsDataURL(file);
+  };
 
-    const changeHandler = (e) => {
-        e.preventDefault();
-        // TODO: 將使用者上傳的圖片轉換成base64 POST到 /api/image-ai { userInput: "" }
+  return (
+    <>
+      <CurrentFileIndicator filePath="/app/vision/page.js" />
+      <PageHeader title="AI Vision" icon={faEye} />
+      <section>
+        <div className="container mx-auto">
+          <label
+            htmlFor="imageUploader"
+            className="inline-block bg-indigo-500 active:bg-indigo-600 px-4 py-2 text-white rounded-md"
+          >
+            Upload Image
+          </label>
+          <input
+            id="imageUploader"
+            type="file"
+            className="hidden"
+            onChange={changeHandler}
+            accept=".jpg, .jpeg, .png"
+          />
+        </div>
+      </section>
+      <section>
+        <div className="container mx-auto">
+          {/* TODO: 顯示AI輸出結果 */}
+          {aiImageUrl && (
+            <image src={aiImageUrl} className="w-72 my-3" alt="使用者上傳" />
+          )}
 
-    }
-
-    return (
-        <>
-            <CurrentFileIndicator filePath="/app/vision/page.js" />
-            <PageHeader title="AI Vision" icon={faEye} />
-            <section>
-                <div className="container mx-auto">
-                    <label htmlFor="imageUploader" className="block">Upload Image</label>
-                    <input
-                        id="imageUploader"
-                        type="file"
-                        onChange={changeHandler}
-                        accept=".jpg, .jpeg, .png"
-                    />
-                </div>
-            </section>
-            <section>
-                <div className="container mx-auto">
-                    {/* TODO: 顯示AI輸出結果 */}
-
-                </div>
-            </section>
-        </>
-    )
+          <h1 className="text-2xl mt-4 font-bold">{aiText}</h1>
+        </div>
+      </section>
+    </>
+  );
 }
